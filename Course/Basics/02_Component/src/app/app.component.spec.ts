a4eae4ac2102ca5a3979827bf410cbff0de2f4f8
Harsh Siddhapura <harsh.siddhapura@indexnine.com>
2022-01-17 12:14:51 +0530

    Update

---
+++ Course/Basics/02_Component/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title '2.Angular-Basics'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('2.Angular-Basics');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('2.Angular-Basics app is running!');
+  });
+});
---
+++ Course/Basics/02_Component/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styles: [`
+    p{
+        padding: 20px;
+        background-color: lightgreen;
+        border: 1px solid green;
+      }
+  `]
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
